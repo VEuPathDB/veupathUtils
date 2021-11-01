b0c51af114008b58b77f82a437619970a32e2402
Danielle Callan <dcallan@upenn.edu>
2021-11-01 15:20:10 -0400

    move some utils out of plot.data

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,127 @@
 # Utils dump
 
+#' Replace Empty String with NULL
+#'
+#' This function replaces the empty string "" with NULL 
+#' @param x character vector
+#' @return non-empty character vector or NULL
+#' @export
+emptyStringToNull <- function(x) {
+  x <- unlist(x)
+  if (is.null(x)) { return(NULL) }
+  if (length(x) == 0) { return(NULL) }
+  if (all(x == "")) { return(NULL) }
+
+  return(as.character(x))
+}
+
+#' POSIXct Test
+#'
+#' This function returns a logical value indicating if x is
+#' a POSIXct object.
+#' @param x an R object
+#' @return logical TRUE if x is a POSIXct object, FALSE otherwise
+#' @export
+is.POSIXct <- function(x) inherits(x, "POSIXct")
+
+#' Trim White Space
+#' 
+#' Function to trim white space from a string.
+#' @param x Character vector to trim white space from.
+#' @return character vector of trimmed values
+#' @export
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+
+#' Non-Zero Rounding
+#' 
+#' This function will recursively attempt to round a value to
+#' greater and greater precision until it results in a non-zero
+#' value. One consequence of this is that the precision of the
+#' output value may not be exactly what was requested.
+#' @param x Numeric value to round
+#' @param digits Number indicating the desired precision
+#' @return number rounded as nearly to the requested precision as 
+#' possible without returning zero.
+#' @export
+nonZeroRound <- function(x, digits) {
+  if (x == 0) {
+    warning("Input is already zero and cannot be rounded to a non-zero number.")
+    return(x)
+  }
+  if (round(x,digits) == 0) { 
+    Recall(x,digits+1) 
+  } else { 
+    round(x,digits) 
+  } 
+}
+
+#' Make Aggregation Formulas
+#' 
+#' This function is intended to help create formulas for input to
+#' base R's `aggregate` function. 
+#' @param valueVars vector of variable/ column names to become the LHS
+#' @param groupingVars vector of variable/ column names to become the RHS
+#' @return character string which can be used as input to `aggregate` with `as.formula`
+#' @export
+getAggStr <- function(valueVars, groupingVars) {
+  valueString <- emptyStringToPoint(paste(valueVars, collapse= " + "))
+  groupingString <- emptyStringToNull(paste(groupingVars, collapse=" + "))
+  aggStr <- paste(c(valueString, groupingString), collapse=" ~ ")
+
+  return(aggStr)
+}
+
+#' @export
+new_data_frame <- function(x = list(), n = NULL) {
+  if (length(x) != 0 && is.null(names(x))) {
+    stop("Elements must be named")
+  }
+  lengths <- vapply(x, length, integer(1))
+  if (is.null(n)) {
+    n <- if (length(x) == 0 || min(lengths) == 0) 0 else max(lengths)
+  }
+  for (i in seq_along(x)) {
+    if (lengths[i] == n) next
+    if (lengths[i] != 1) {
+      stop("Elements must equal the number of rows or 1")
+    }
+    x[[i]] <- rep(x[[i]], n)
+  }
+
+  class(x) <- "data.frame"
+
+  attr(x, "row.names") <- .set_row_names(n)
+  x
+}
+
+#' Fast data.frame
+#' 
+#' This function should be used with extreme caution. If data.frame is not 
+#' performant enough, you should consider data.table instead. This is 
+#' intended to meet a very specific use case where a solution is needed which
+#' is both efficient and tidy (meaning tidyverse compatible). It comes at the
+#' cost of cutting some corners. There is no checking, recycling etc. unless asked for.
+#' @param ... usual arguments to data.frame
+#' @export
+data_frame <- function(...) {
+  new_data_frame(list(...))
+}
+
+#' Efficient String Split
+#'
+#' This function efficiently splits strings by a vectorized approach.
+#' @param str Character vector or list of strings to split
+#' @param pattern Character string or regular expression to split str on
+#' @param ncol Number of anticipated output values/ columns after splitting
+#' @param index Number indicating index of value/ column of interest
+#' @param fixed Boolean indicating if `pattern` is a literal string (TRUE) 
+#' or regular expression (FALSE)
+#' @return Character vector of substrings
+#' @export
+strSplit <- function(str, pattern, ncol = 2, index = 1, fixed = TRUE) {
+  matrix(unlist(strsplit(str, pattern, fixed = fixed)), ncol = ncol, byrow = TRUE)[,index]
+}
+
 #' Try-error Test
 #'
 #' This function returns a logical value indicating if x is
