a9fbb0391e7869ecedbe63b3638261180cd1920a
asizemore <annsize@seas.upenn.edu>
2022-02-01 15:37:01 -0500

    change setNaToValue to setNaToZero

--- R/utils-numeric.R
+++ R/utils-numeric.R
@@ -24,25 +24,24 @@ nonZeroRound <- function(x, digits) {
 
 #' Impute 0s for numeric columns
 #'
-#' This function replaces NAs in numeric columns of a data.table
-#' with a desired value.
+#' This function replaces NAs in numeric columns of a data.table with 0.
 #' @param dt data.table
-#' @param value Numeric value to use when replacing NAs. Default is 0.
 #' @param cols Vector of column names for which the NA replacement should occur.
-#' Defulat is all numeric columns.
+#' Default is all numeric columns.
 #' @return inputted data.table dt but with NAs replaced with desired value.
 #' @export
-setNaToValue <- function(dt, value = 0, cols = NULL) {
+setNaToZero <- function(dt, cols = NULL) {
 
   # if cols not set, use all numeric cols
   if (is.null(cols)) {
     cols <- colnames(dt)[sapply(dt, class) == "numeric"]
   }
+  
   # Ensure data.table
   if (!'data.table' %in% class(dt)) {
     data.table::setDT(dt)
   }
 
-  data.table::setnafill(dt, fill = value, cols = cols)
+  data.table::setnafill(dt, fill = 0, cols = cols)
 
 }
--- tests/testthat/test-utils-numeric.R
+++ tests/testthat/test-utils-numeric.R
@@ -8,7 +8,7 @@ test_that("nonZeroRound only returns 0 if it receives one", {
   expect_equal(nonZeroRound(0.00000019, 4), 0.0000002)
 })
 
-test_that("setNaToValue replaces intended NAs", {
+test_that("setNaToZero replaces intended NAs", {
   
   df <- iris
 
@@ -17,13 +17,13 @@ test_that("setNaToValue replaces intended NAs", {
   df <- lapply(df, function(x) {x[sample(1:length(x), size=nMissing)] <- NA; return(x)})
 
   # With specified columns
-  setNaToValue(df, value=1000, cols=c("Sepal.Length", "Sepal.Width"))
+  setNaToZero(df, cols=c("Sepal.Length", "Sepal.Width"))
   expect_equal(sum(is.na(df)), 3*nMissing)
-  expect_equal(sum(df[, c('Sepal.Length', 'Sepal.Width')] == 1000), 2*nMissing)
+  expect_equal(sum(df[, c('Sepal.Length', 'Sepal.Width')] == 0), 2*nMissing)
 
   # With defualt. Should change only numeric columns
-  setNaToValue(df, value=500)
+  setNaToZero(df)
   expect_equal(sum(is.na(df)), nMissing)
-  expect_equal(sum(df[, c('Petal.Length', 'Petal.Width')] == 500), 2*nMissing)
+  expect_equal(sum(df[, c('Petal.Length', 'Petal.Width')] == 0), 2*nMissing)
 
 })
