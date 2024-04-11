842c03d2ad6ea8c902a91013720f63ce684fcb11
Danielle Callan <dcallan@upenn.edu>
2024-04-10 21:30:04 -0400

    remove unused collections entity id slot from megastudy class

--- R/class-Megastudy.R
+++ R/class-Megastudy.R
@@ -74,23 +74,10 @@ check_megastudy <- function(object) {
     errors <- c(errors, msg)
   }
 
-  if (!!length(object@collectionIdColumn)) {
-    
-    if (length(object@collectionIdColumn) != 1) {
-      msg <- paste("Collection ID column must have a single value.")
+  if (!!length(object@collectionsDT)) {
+    if (!all(ancestor_id_cols[1:length(ancestor_id_cols)-1] %in% names(object@collectionsDT))) {
+      msg <- paste("Not all ancestor ID columns are present in collection data.frame")
       errors <- c(errors, msg)
-    }
-
-    if (!object@collectionIdColumn %in% ancestor_id_cols) {
-      msg <- paste("Collection ID column must be an ancestor ID column")
-      errors <- c(errors, msg)
-    }
-
-    if (!!length(object@collectionsDT)) {
-      if (!any(object@collectionsDT[[object@collectionIdColumn]] %in% df[[object@collectionIdColumn]])) {
-        msg <- paste("Collection IDs not found in data.frame. At least some of the provided collection IDs should be present in the data.frame.")
-        errors <- c(errors, msg)
-      }
     }
   }
 
@@ -107,8 +94,6 @@ check_megastudy <- function(object) {
 #' @slot data A data.table
 #' @slot ancestorIdColumns A character vector of column names representing parent entities of the recordIdColumn.
 #' @slot studySpecificVocabularies veupathUtils::StudySpecificVocabulariesByVariableList
-#' @slot collectionIdColumn The name of the column in the data.frame that contains the collection ids. 
-#' The collectionId should also be a member of ancestorIdColumns.
 #' @slot collectionIds A character vector of collection ids we expect. If none provided, they will be inferred from those present.
 #' 
 #' @name Megastudy-class
@@ -118,6 +103,5 @@ Megastudy <- setClass("Megastudy", representation(
     data = 'data.table',
     ancestorIdColumns = 'character',
     studySpecificVocabularies = 'StudySpecificVocabulariesByVariableList',
-    collectionIdColumn = 'character',
     collectionsDT = 'data.frame'
 ), validity = check_megastudy)
